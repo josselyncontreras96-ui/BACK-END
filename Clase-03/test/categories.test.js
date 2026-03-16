ad66c6e10f13a8706137af60034c8615b659db83
Josselyn Contreras <josselyn.contreras96@gmail.com>
2026-03-16 22:00:53 +0100

    PUT Y DELETE

--- Clase-03/test/categories.test.js
+++ Clase-03/test/categories.test.js
@@ -2,14 +2,25 @@ import { expect } from "chai";
 import request from "supertest";
 import app from "../app.js";
 
+import Category from "../models/Category.js";
+
 describe("Categories endpoint", () => {
-  it("debería tener un status 200 y un array", async function () {
+  beforeEach(async function () {
+    await Category.deleteMany({});
+
+    await Category.create({
+      name: "Electronics",
+    });
+  });
+
+  it("Categoría debería tener un status 200 y un array", async function () {
     const res = await request(app).get("/categories");
 
     // console.log(res.status, res.body);
 
     expect(res.status).to.equal(200);
     expect(res.body).to.be.an("array");
+    expect(res.body.length).to.equal(1);
   });
 
   it("should return name categories ", async function () {
@@ -23,6 +34,7 @@ describe("Categories endpoint", () => {
       name: "Books",
       description: "All kinds of books",
     };
+
     const response = await request(app).post("/categories").send(newCategory);
 
     expect(response.status).to.equal(201);
@@ -30,41 +42,89 @@ describe("Categories endpoint", () => {
     expect(response.body.name).to.equal("Books");
   });
 
-  it("Debería traer una categoría por el ID", async function () {
-    const newCategory = {
-      name: "Books",
-      description: "All kinds of books",
-    };
-    const responseCategory = await request(app)
-      .post("/categories")
-      .send(newCategory);
+  // it("Debería traer una categoría por el ID", async function () {
+  //   const category = await Category.findOne({ name: "Electronics" });
 
-    expect(responseCategory.status).to.equal(201);
-    expect(responseCategory.body).to.have.property("name");
-    expect(responseCategory.body.name).to.equal("Books");
+  //   const res = await request(app).get(`/categories/${category.id}`);
 
-    // Test
-  });
+  //   expect(res.status).to.equal(200);
+  //   expect(res.body).to.have.property("name");
+  //   expect(res.body.name).to.equal("Electronics");
+  // });
 
   it("Debería mostrar una categoría por su id", async function () {
-    const newCategory = {
-      name: "Categoría",
-    //   type: "69b44f1436c7b2402be86f5f",
-      description: "Categoría de test",
-    };
+    // const newCategory = {
+    //   name: "Categoría",
+    //   description: "Categoría de test",
+    // };
 
-    const res = await request(app).post("/categories").send(newCategory);
+    // const res = await request(app).post("/categories").send(newCategory);
 
-    expect(res.status).to.equal(201);
-    expect(res.body).to.have.property("name");
-    expect(res.body).to.have.property("description");
-    expect(res.body.name).to.equal("Categoría");
-    expect(res.body.description).to.equal("Categoría de test");
+    // expect(res.status).to.equal(201);
+    // expect(res.body).to.have.property("name");
+    // expect(res.body).to.have.property("description");
+    // expect(res.body.name).to.equal("Categoría");
+    // expect(res.body.description).to.equal("Categoría de test");
+
+    const category = await Category.findOne({ name: "Electronics" });
 
     const response = await request(app).get(`/categories/${res.body._id}`);
 
     expect(response.status).to.equal(200);
     expect(response.body).to.have.property("name");
-    expect(response.body.name).to.equal("Categoría");
+    expect(response.body.name).to.equal("Electronics");
   });
+  
+it("Categories Deberia devolver 422 si falta el nombre", async function () {
+
+  const newCategory = {
+    description: "Productos de oficina"
+  };
+
+  const res = await request(app)
+    .post("/categories")
+    .send(newCategory);
+
+  expect(res.status).to.equal(422);
+
 });
+it("Debería actualizar una categoría", async function () {
+
+  const category = await Category.findOne();
+
+  const updatedCategory = {
+    name: "Categoría actualizada"
+  };
+
+  const res = await request(app)
+    .put(`/categories/${category._id}`)
+    .send(updatedCategory);
+
+  expect(res.status).to.equal(200);
+  expect(res.body.name).to.equal("Categoría actualizada");
+
+});
+
+it("Deberia borrar una categoria", async function () {
+
+  const category = await Category.findOne();
+
+  const res = await request(app)
+    .delete(`/categories/${category._id}`);
+
+  expect(res.status).to.equal(200);
+
+});
+
+it("Deberia devolver 404 si la categoria no existe", async function () {
+
+  const fakeId = "507f1f77bcf86cd799439011";
+
+  const res = await request(app)
+    .delete(`/categories/${fakeId}`);
+
+  expect(res.status).to.equal(404);
+
+});
+
+})
--- Clase-03/test/products.test.js
+++ Clase-03/test/products.test.js
@@ -8,6 +8,23 @@ import Product from "../models/Product.js";
 describe("Products endpoint", function () {
   this.timeout(10000);
 
+  this.beforeEach(async function () {
+    await Category.deleteMany({});
+
+    const category = await Category.create({
+      name: "Electronica",
+    });
+    
+    await Product.deleteMany({}); // con este elimina lo que craeamos cada vez que hacemos el test
+
+    await Product.create ({
+      name: "Mouse",
+      price: 80,
+      stock: 10,
+      category: category._id,
+    });
+  });
+
   it("debería tener un status 200 y un array", async function () {
     const res = await request(app).get("/products");
 
@@ -15,6 +32,7 @@ describe("Products endpoint", function () {
 
     expect(res.status).to.equal(200);
     expect(res.body).to.be.an("array");
+    expect(res.body.length).to.equal(1);
   });
 
   it("El primer producto tiene que tener nombre", async function () {
@@ -24,7 +42,7 @@ describe("Products endpoint", function () {
   });
 
   it("Debería crear un producto", async function () {
-    const category = await Category.create({ name: "Electronics" });
+    const category = await Category.findOne({ name: "Electronica" });
 
     const newProduct = {
       name: "Notebook",
@@ -42,42 +60,70 @@ describe("Products endpoint", function () {
 
   it("Debería traer un producto por el id", async function () {
     // Crear una categoría para asociar al producto
-    const newCategory = {
-      name: "Books",
-      description: "All kinds of books",
-    };
-
-    const responseCategory = await request(app)
-      .post("/categories")
-      .send(newCategory);
-
-    expect(responseCategory.status).to.equal(201);
-    expect(responseCategory.body).to.have.property("name");
-    expect(responseCategory.body.name).to.equal("Books");
-
-    // Crear un producto asociado a la categoría creada
-    const newProduct = {
-      name: "Notebook",
-      price: 1000,
-      stock: 5,
-      category: responseCategory.body._id,
-    };
-
-    const responseProduct = await request(app)
-      .post("/products")
-      .send(newProduct);
-
-    expect(responseProduct.status).to.equal(201);
-    expect(responseProduct.body).to.have.property("name");
-    expect(responseProduct.body.name).to.equal("Notebook");
-
-    // Empieza el test
+    const product = await Product.findOne();
     const response = await request(app).get(
-      `/products/${responseProduct.body._id}`,
+      `/products/${product.id}`,
     );
 
     expect(response.status).to.equal(200);
     expect(response.body).to.have.property("name");
-    expect(response.body.name).to.equal("Notebook");
+    expect(response.body.name).to.equal("Mouse");
   });
+
+it("Deberia devolver 422 si falta el nombre", async function () {
+  const category = await Category.findOne();
+
+const newProduct = {
+  price: 1000,
+  stock: 5,
+  category: category.id,
+};
+const res = await request(app).post("/products").send(newProduct);
+expect(res.status).to.equal(422);
+});
+
+
+
+it("Deberia actualizar un producto", async function () {
+
+  const product = await Product.findOne();
+
+  const updateProduct = {
+    name: "Mouse Gamer",
+    category: product.category,
+  };
+
+  const res = await request(app)
+    .put(`/products/${product.id}`)
+    .send(updateProduct);
+
+  expect(res.status).to.equal(200);
+  expect(res.body.name).to.equal("Mouse Gamer");
+
+});
+it("Deberia borrar un producto", async function () {
+
+  const product = await Product.findOne();
+
+  const res = await request(app)
+    .delete(`/products/${product._id}`);
+
+  expect(res.status).to.equal(204);
+
+  const deletedProduct = await Product.findById(product._id);
+  expect(deletedProduct).to.equal(null);
+
+});
+
+it("Deberia devolver 404 si el producto no existe", async function () {
+
+  const fakeId = "507f1f77bcf86cd799439011";
+
+  const res = await request(app)
+    .delete(`/products/${fakeId}`);
+
+  expect(res.status).to.equal(404);
+
+});
+
 });
