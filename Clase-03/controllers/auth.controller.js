2c90d34e8ffaf883e2c3c2d6d68112abb9ba1d60
Josselyn Contreras <josselyn.contreras96@gmail.com>
2026-03-31 16:39:08 +0200

    validacion de usuario

--- Clase-03/controllers/auth.controller.js
+++ Clase-03/controllers/auth.controller.js
@@ -2,7 +2,35 @@ import User from "../models/user.js"
 import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
-    const { email, password } = req.body;
+    try {
+            const { email, password } = req.body;
+    console.log(email.includes("@"));
+
+    if (!email || !password) {
+        return res.status(400).json({ error: "correo y contraseña son requeridos" });
+    }
+
+    /* if (!email.includes("@")) {
+        return res.status(400).json({ error: "correo no es valido" });
+ }*/
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// console.log(emailRegex.test(email));
+
+if (!emailRegex.test(email)) {
+  return res.status(400).json({ error: "correo no es valido" });
+}
+if (password.length < 6) {
+    return res.status(400).json({ error: "contraseña debe tener al menos 6 caracteres" });
+}
+
+const existingUser = User.findOne({ email});
+if (existingUser) {
+    return res.status(400).json({ error: " Usuariro duplicado"});
+}
+
+return res.send("Probando");
+
 
     const hash = await bcrypt.hash(password, 10);
 
@@ -11,10 +39,13 @@ export const register = async (req, res) => {
          password: hash,
         });
 
-        
+
 
     res.status(201).json({
         id: user.id,
         email: user.email,
-    });     
+    }); 
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 };
